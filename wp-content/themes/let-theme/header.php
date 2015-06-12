00d9b113329896e7dbb724bcea50c0f161dc00a8
Alberto Barradas <abcsds@gmail.com>
2015-06-12 12:59:33 -0500

    Header

--- wp-content/themes/let-theme/header.php
+++ wp-content/themes/let-theme/header.php
@@ -6,11 +6,35 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' ) ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta name="viewport" content="width=device-width">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- TODO: Relative paths! -->
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+
         <title><?php wp_title( '|', true, 'right' ) ?></title>
-		<meta name="author" content="">
-		<link rel="author" href="">
+		<meta name="author" content="Alberto Barradas">
+		<link rel="author" href="http://github.com/abcsds">
 		<?php wp_head() ?>
+
+        <!-- Bootstrap Core CSS -->
+        <!-- TODO: Relative paths! -->
+        <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">
+
+        <!-- Custom Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+        <!-- TODO: Relative paths! -->
+        <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css" type="text/css">
+
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
     </head>
     <body <?php body_class() ?>>
 		<header id="page-header">
