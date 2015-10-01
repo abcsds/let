9d247302051606aee9403dc993997e3c9d2094ea
Alberto Barradas <abcsds@gmail.com>
2015-10-01 14:54:19 -0500

    Remove mecate-en theme

--- wp-content/themes/mecate-en/404.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-	<div id="page-content" class="row">
-		<div class="jumbotron col-md-12">
-			<h1>Error 404</h1>
-			<?php get_search_form(); ?>
-		</div>
-	</div>
-<?php get_footer(); ?>
--- wp-content/themes/mecate-en/page.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-<div id="page-content">
-	<?php get_template_part('loop', 'page'); ?>
-</div>
-<?php get_footer(); ?>
